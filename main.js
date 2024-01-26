4ba96cf213a57c83976727227d10ba32e17755e5
flapmfy <frantis3k.kasik@gmail.com>
2024-01-26 18:54:55 +0100

    Color change on scroll

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
 import './style.css';
 
+const issues = document.querySelectorAll('.issue');
+const body = document.querySelector('body');
+const primaryNav = document.querySelector('.primary-nav');
+const issueLinks = document.querySelectorAll('.issue-link');
+
+const oberver = new IntersectionObserver(issueIsVisible, { threshold: 0.2 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  issues.forEach((issue) => oberver.observe(issue));
+});
+
+function issueIsVisible(entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const intersectionElement = entry.target;
+      setActiveLink(intersectionElement.id);
+      changeTheme(intersectionElement.dataset.theme);
+    }
+  });
+}
+
+function changeTheme(themeName) {
+  body.className = `theme--${themeName}`;
+}
+
+function setActiveLink(id) {
+  issueLinks.forEach((issueLink) => issueLink.classList.remove('active'));
+
+  let activeLink = primaryNav.querySelector(`a[href="#${id}"]`);
+  activeLink.classList.add('active');
+}
+
